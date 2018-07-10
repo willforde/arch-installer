781ee622bb47c20bc3eb6f18c46d7e5dfb108f7b
William Forde <willforde@gmail.com>
2018-07-10 16:50:07 +0100

    add video-decoder script

--- scripts/video-decoder.sh
+++ scripts/video-decoder.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 
-libva-intel-driver
+# Check if system is using a nvidia VGA device and if so, mark nvidia driver for install
+if [[ -n $(lspci | grep -i "VGA compatible controller: NVIDIA Corporation") ]]; then
+    sudo pacman -S --noconfirm --needed nvidia libva-vdpau-driver
+
+elif [[ -n $(lspci | grep -i "VGA compatible controller: Intel Corporation") ]]; then
+    sudo pacman -S --noconfirm --needed libva-intel-driver libvdpau-va-gl
+fi
+
