a6795fb05e3ade0b3a32777e80d32cb7e449753c
William Forde <willforde@gmail.com>
2018-07-12 18:36:48 +0100

    fix mkinitcpio script

--- scripts/mkinitcpio_hooks.sh
+++ scripts/mkinitcpio_hooks.sh
@@ -3,11 +3,18 @@
 # Replace Hooks with custom set of hooks
 # This mainly just removes fsck & keyboard from hooks
 echo "Removing fsck & keyboard hooks"
-sudo sed -Ei 's/^HOOKS=".+"/HOOKS="base udev autodetect modconf block filesystems"/' /etc/mkinitcpio.conf
-
-# Remove fallback preset from mkinitcpio
+sudo sed -E 's/^HOOKS=(.+)/HOOKS=(base udev autodetect modconf block filesystems)/' /etc/mkinitcpio.conf
 echo ""
-sudo sed -i "s/^PRESETS=('default' 'fallback')/PRESETS=('default')/" /etc/mkinitcpio.d/linux.preset
+
+# Remove fallback preset from linux mkinitcpio
+if [ -f "/etc/mkinitcpio.d/linux.preset" ]; then
+    sudo sed -i "s/^PRESETS=('default' 'fallback')/PRESETS=('default')/" /etc/mkinitcpio.d/linux.preset
+fi
+
+# Remove fallback preset from lts mkinitcpio
+if [ -f "/etc/mkinitcpio.d/linux-lts.preset" ]; then
+    sudo sed -i "s/^PRESETS=('default' 'fallback')/PRESETS=('default')/" /etc/mkinitcpio.d/linux-lts.preset
+fi
 
 # Rebuild mkinitcpio
 sudo mkinitcpio -P
