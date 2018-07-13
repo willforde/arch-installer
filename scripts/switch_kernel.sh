fa8bfa644f7cc503304787b63da7285891820e05
William Forde <willforde@gmail.com>
2018-07-13 01:30:03 +0100

    add switch_kernel.sh

---
+++ scripts/switch_kernel.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+set -e
+set -u
+
+files=$(dirname $0)/files
+source ${files}/support.sh
+
+if [ "$#" == 1 ]; then
+    if [ "$1" == "linux" ]; then
+        sudo pacman -S --noconfirm --needed linux
+        echo "Setting refind to load lts kernel"
+        sudo sed -i 's|initrd=/boot/initramfs-linux-lts.img|initrd=/boot/initramfs-linux.img|' /boot/refind_linux.conf
+
+        # Remove linux lts if exists
+        if [ -f "/boot/vmlinuz-linux-lts" ]; then
+            sudo pacman -R --noconfirm linux-lts
+            if [ -f "/etc/mkinitcpio.d/linux-lts.preset.pacsave" ]; then
+                sudo rm -v /etc/mkinitcpio.d/linux-lts.preset.pacsave
+            fi
+        fi
+
+        # Remove fallback kernel images
+        removefallback
+
+    elif [ "$1" == "lts" ]; then
+        sudo pacman -S --noconfirm --needed linux-lts
+        echo "Setting refind to load lts kernel"
+        sudo sed -i 's|initrd=/boot/initramfs-linux.img|initrd=/boot/initramfs-linux-lts.img|' /boot/refind_linux.conf
+
+        # Remove linux lts if exists
+        if [ -f "/boot/vmlinuz-linux" ]; then
+            sudo pacman -R --noconfirm linux
+            if [ -f "/etc/mkinitcpio.d/linux.preset.pacsave" ]; then
+                sudo rm -v /etc/mkinitcpio.d/linux.preset.pacsave
+            fi
+        fi
+
+        # Remove fallback kernel images
+        removefallback
+
+    else
+        echo "Unknown kernel type specified, options are: linux, lts"
+    fi
+else
+    echo "Please specify kernel type, options are: linux, lts"
+fi
