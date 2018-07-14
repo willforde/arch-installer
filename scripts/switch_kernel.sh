cc91a2a0bee2cf64ace1490846c526876ed44e6a
William Forde <willforde@gmail.com>
2018-07-14 01:02:05 +0100

    add kernel headers

--- scripts/switch_kernel.sh
+++ scripts/switch_kernel.sh
@@ -7,13 +7,13 @@ source ${files}/support.sh
 
 if [ "$#" == 1 ]; then
     if [ "$1" == "linux" ]; then
-        sudo pacman -S --noconfirm --needed linux
+        sudo pacman -S --noconfirm --needed linux linux-headers
         echo "Setting refind to load lts kernel"
         sudo sed -i 's|initrd=/boot/initramfs-linux-lts.img|initrd=/boot/initramfs-linux.img|' /boot/refind_linux.conf
 
         # Remove linux lts if exists
         if [ -f "/boot/vmlinuz-linux-lts" ]; then
-            sudo pacman -R --noconfirm linux-lts
+            sudo pacman -R --noconfirm linux-lts linux-lts-headers
             if [ -f "/etc/mkinitcpio.d/linux-lts.preset.pacsave" ]; then
                 sudo rm -v /etc/mkinitcpio.d/linux-lts.preset.pacsave
             fi
@@ -23,13 +23,13 @@ if [ "$#" == 1 ]; then
         removefallback
 
     elif [ "$1" == "lts" ]; then
-        sudo pacman -S --noconfirm --needed linux-lts
+        sudo pacman -S --noconfirm --needed linux-lts linux-lts-headers
         echo "Setting refind to load lts kernel"
         sudo sed -i 's|initrd=/boot/initramfs-linux.img|initrd=/boot/initramfs-linux-lts.img|' /boot/refind_linux.conf
 
         # Remove linux lts if exists
         if [ -f "/boot/vmlinuz-linux" ]; then
-            sudo pacman -R --noconfirm linux
+            sudo pacman -R --noconfirm linux linux-headers
             if [ -f "/etc/mkinitcpio.d/linux.preset.pacsave" ]; then
                 sudo rm -v /etc/mkinitcpio.d/linux.preset.pacsave
             fi
