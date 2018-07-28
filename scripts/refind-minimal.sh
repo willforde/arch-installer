99149cbe19db61b0d1f33977bf1144a52193d655
William Forde <willforde@gmail.com>
2018-07-28 22:13:38 +0100

    more refind themes

--- scripts/refind-minimal.sh
+++ scripts/refind-minimal.sh
@@ -2,7 +2,7 @@
 set -e
 set -u
 
-# Download rEFInd-black theme and customize it
+# Download rEFInd-minimal theme and customize it
 # Then configure rEFInd to use it
 # https://github.com/anthon38/refind-black
 
@@ -13,35 +13,35 @@ if [[ $EUID -ne 0 ]]; then
 fi
 
 # Remove old refind-black directory if it already exists, this allows for updating
-if [ -d "/boot/efi/EFI/refind/themes/rEFInd-black" ]; then
-    echo "Removing old rEFInd-black..."
-    rm -rf /boot/efi/EFI/refind/themes/rEFInd-black
+if [ -d "/boot/efi/EFI/refind/themes/rEFInd-minimal" ]; then
+    echo "Removing old rEFInd-minimal..."
+    rm -rf /boot/efi/EFI/refind/themes/rEFInd-minimal
 else
     # Make sure that the themes directory exists
     mkdir -p /boot/efi/EFI/refind/themes/
 fi
 
 # Download a copy of the refind theme
-echo "Downloading rEFInd-black..."
+echo "Downloading rEFInd-minimal..."
 cd /boot/efi/EFI/refind/themes/
-curl -SLO https://github.com/anthon38/refind-black/archive/master.tar.gz
+curl -SLO https://github.com/EvanPurkhiser/rEFInd-minimal/archive/master.tar.gz
 tar zxf master.tar.gz
 rm -f master.tar.gz
-mv refind-black-master rEFInd-black
+mv rEFInd-minimal-master rEFInd-minimal
 
 # Cleanup
 echo ""
-rm -fv rEFInd-black/README.md
-rm -fv rEFInd-black/theme.conf
+rm -fv rEFInd-minimal/README.md
+rm -fv rEFInd-minimal/theme.conf
 
 # Create a slightly custom version of rEFInd-black theme.conf
 echo "Creating custom theme.conf"
-cat > rEFInd-black/theme.conf <<EOF
+cat > rEFInd-minimal/theme.conf <<EOF
 hideui singleuser,hints,arrows,label
-#icons_dir themes/rEFInd-black/icons
-banner themes/rEFInd-black/background.png
-selection_big   themes/rEFInd-black/selection_big.png
-selection_small themes/rEFInd-black/selection_small.png
+#icons_dir themes/rEFInd-minimal/icons
+banner themes/rEFInd-minimal/background.png
+selection_big   themes/rEFInd-minimal/selection_big.png
+selection_small themes/rEFInd-minimal/selection_small.png
 use_graphics_for osx,linux,windows
 showtools
 timeout 2
@@ -49,7 +49,7 @@ EOF
 
 # Add include line to refind.conf if it don't exist, to enable this theme
 if grep -Exq "include themes/rEFInd-\w+/theme.conf" ../refind.conf; then
-    sed -Ei "s|include themes/rEFInd-\w+/theme.conf|include themes/rEFInd-black/theme.conf|" refind.conf
+    sed -Ei "s|include themes/rEFInd-\w+/theme.conf|include themes/rEFInd-minimal/theme.conf|" refind.conf
 else
-    echo 'include themes/rEFInd-black/theme.conf' >> ../refind.conf
+    echo 'include themes/rEFInd-minimal/theme.conf' >> ../refind.conf
 fi
