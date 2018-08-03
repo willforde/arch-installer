0bb393567d86e1caf54be7fb14b2ca9186ad88bc
willforde <willforde@gmail.com>
2018-08-04 00:34:13 +0100

    remove cpToUsers

--- scripts/files/support.sh
+++ scripts/files/support.sh
@@ -2,24 +2,6 @@
 set -e
 set -u
 
-cpToUsers()
-{
-    # Copy one file to every users home directory
-    # arg1 = Source file to copy
-    # arg2 = Path source will be copied to, path is relitive to user directory
-
-    for user in $(cat /etc/passwd | grep "/home/" | grep -E "/bin/bash|/bin/zsh" | awk -F ":" '{print $1}'); do
-        dest="/home/$user/$2"
-        DIR=$(dirname "${dest}")
-        if [ ! -d ${DIR} ]; then
-            sudo mkdir -p ${DIR}
-            sudo chown 700 ${DIR}
-            sudo chown ${user}:${user} ${DIR}
-        fi
-        sudo install -vm 644 $1 ${dest}
-        sudo chown ${user}:${user} ${dest}
-    done
-}
 
 chkShell()
 {
